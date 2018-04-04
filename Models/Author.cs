22e118bc3582d0bf99e85cc40c7cae12e78afa6e
ChiefKeef07 <keefer.alex@yahoo.com>
2018-04-04 18:52:14 -0400

    simple change

--- Models/Author.cs
+++ Models/Author.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Fisher.Bookstore.Api.Data
 {
+    //Authors Model
     public class Author{
         public int AuthorId { get; set; }
         public string Name { get; set; }
